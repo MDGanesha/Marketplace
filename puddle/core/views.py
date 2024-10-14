3f236fedd03547c0af172b70e63b7f5eaf135979
MDGanesha <ganeshamd24@gmail.com>
2024-10-14 22:34:20 +0530

    sign up,login,new

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from .models import Category,Item
+from django.shortcuts import render,redirect
+from item.models import Category,Item
+from .forms import SignupForm
 # Create your views here.
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_sold=False)[0:4]
     categories = Category.objects.all()
-    context = {'categories':categories,'items':items}
-    return render(request,'core/index.html',context)
+    return render(request,'core/index.html',{'categories':categories,'items':items})
 def contact(request):
     return render(request,'core/contact.html')
 
+def signup(request):
+    if request.method=='POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    form = SignupForm()
+
+    return render(request,'core/signup.html',{'form':form})
--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-)9gjd_plwpe1ib5$#rwnm8q^@el=pn%q6s_r#@qqj(p@4w6e#v
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    'core',
     'item',
 ]
 
